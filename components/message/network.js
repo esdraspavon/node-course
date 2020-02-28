8e2b4c3193f0eb21ae638be85abcce8dacc7c4f1
Esdras Pavon <pavonesdras@gmail.com>
2020-02-28 16:39:11 -0300

    added post and get messages

--- components/message/network.js
+++ components/message/network.js
@@ -1,30 +1,26 @@
 const express = require("express");
 const router = express.Router();
-
+const controller = require('./controller')
 const response = require("../../network/response");
 
 router.get("/", function(req, res) {
-  console.log(req.headers);
-  //   res.header({
-  //     "custom-header": "Nuestro valor personalizado"
-  //   });
-  //   res.send("Hola desde get");
-  response.success(req, res, "Lista de mensajes");
+  controller.getMessages()
+  .then((messageList) => {
+    response.success(req, res, messageList, 200);
+  })
+  .catch(e => {
+    response.error(req, res, 'Unexpected Error', 500, e)
+  });
 });
 router.post("/", function(req, res) {
-  console.log(req.query.error);
-  //   res.status(201).send({ error: "", body: "Creado correctamente" });
-  if (req.query.error) {
-    response.error(
-      req,
-      res,
-      "Error inesperado",
-      500,
-      "Es solo una simulacion de los errores"
-    );
-  } else {
-    response.success(req, res, "Creado correctamente", 201);
-  }
+
+  controller.addMessage(req.body.user, req.body.message)
+  .then((fullMessage)=> {
+    response.success(req, res, fullMessage, 201);
+  })
+  .catch((e) => {
+    response.error(req, res, "Información inválida", 400, 'Error en el controlador');
+  });
 });
 
 module.exports = router;
